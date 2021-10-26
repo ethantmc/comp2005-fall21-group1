a54958bdf104b5c925cb695fa16a29f008406496
ethantmc <77448566+ethantmc@users.noreply.github.com>
2021-10-26 16:29:39 -0230

    Update Turn.java
    method to actually pick a player to go first now implemented.

--- Group 1 Game of Focus/src/Turn.java
+++ Group 1 Game of Focus/src/Turn.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Turn {
 private Player currentPlayer;
+private ArrayList<Player> listOfPlayers;
 
 public String getCurrentPlayer() {
 		return Player.getName();
@@ -8,5 +11,11 @@ public String getCurrentPlayer() {
 
 public void setCurrentPlayer(Player currentPlayer) {
 	this.currentPlayer = currentPlayer;
+}
+
+private void initiatePlayerTurn() {
+	int playerTurn;
+	playerTurn = ThreadLocalRandom.current().nextInt(1, 5); //Selects from [1,5)
+	currentPlayer = listOfPlayers.get(playerTurn);
 }
 }
