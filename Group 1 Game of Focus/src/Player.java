d2b840e22d6f9eeda53023a66a6d8d47e0bc0ff7
ethantmc <77448566+ethantmc@users.noreply.github.com>
2021-10-31 21:50:15 -0230

    Partial Deliverable: Stack-Token Communication
    This is, I think, complete. As much as it can be until other partial deliverables catch up, anyways.

--- Group 1 Game of Focus/src/Player.java
+++ Group 1 Game of Focus/src/Player.java
@@ -8,61 +8,71 @@ public class Player {
 	private Color color;
 	private int reserveCount = 13;
 	private int capturedCount = 0;
-	private int ownTokensCaptured= 0; //IMO, rename to tokensLost
-	
+	private int piecesLost= 0; //IMO, rename to tokensLost
+
 	public Player(String name, PlayerType type, Boolean colorblindSetting) {
 		super();
 		this.name = name;
 		this.type = type;
 		this.isColorblindEnabled = colorblindSetting;
 	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public PlayerType getType() {
-		return type;
-	}
-	public void setType(PlayerType type) {
-		this.type = type;
-	}
-	public Boolean getColorblindSetting() {
-		return isColorblindEnabled;
-	}
-	public void setColorblindSetting(Boolean colorblindSetting) {
-		this.isColorblindEnabled = colorblindSetting;
-	}
-	public void toggleColorblindSetting() 
-	{
-		if(this.isColorblindEnabled)
-			this.isColorblindEnabled = false;
-		else
-			this.isColorblindEnabled = true;
-	}
-	public int getReserveCount() {
-		return reserveCount;
-	}
-	public void setReserveCount(int reserveCount) {
-		this.reserveCount = reserveCount;
-	}
 	public int getCapturedCount() {
 		return capturedCount;
-	}
-	public void setCapturedCount(int capturedCount) {
-		this.capturedCount = capturedCount;
-	}
-	public int getOwnTokensCaptured() {
-		return ownTokensCaptured;
-	}
-	public void setOwnTokensCaptured(int ownTokensCaptured) {
-		this.ownTokensCaptured = ownTokensCaptured;
 	}
 	public Color getColor() {
 		return color;
 	}
+	public Boolean getColorblindSetting() {
+		return isColorblindEnabled;
+	}
+	public String getName() {
+		return name;
+	}
+	public int getPiecesLost() {
+		return piecesLost;
+	}
+	public int getReserveCount() {
+		return reserveCount;
+	}
+	public PlayerType getType() {
+		return type;
+	}
+	public void incrementCapturedCount(int number) {
+		this.capturedCount += number; //+= works in java AFAIK.
+	}
+	public void incrementPiecesLost(int number) {
+		this.piecesLost += number;
+	}
+	public void incrementReserveCount(int number) {
+		this.reserveCount += number;
+	}
+	public void setCapturedCount(int capturedCount) {
+		this.capturedCount = capturedCount;
+	}
 	public void setColor(Color color) {
 		this.color = color;
+	}
+	public void setColorblindSetting(Boolean colorblindSetting) {
+		this.isColorblindEnabled = colorblindSetting;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setPiecesLost(int number) {
+		this.piecesLost = number;
+	}
+	public void setReserveCount(int reserveCount) {
+		this.reserveCount = reserveCount;
+	}
+	public void setType(PlayerType type) {
+		this.type = type;
+	}
+	public void toggleColorblindSetting() 
+	{
+		if(this.isColorblindEnabled) {
+			this.isColorblindEnabled = false;
+		} else {
+			this.isColorblindEnabled = true;
+		}
 	}
 }
--- Group 1 Game of Focus/src/Turn.java
+++ Group 1 Game of Focus/src/Turn.java
@@ -2,20 +2,20 @@ import java.util.ArrayList;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Turn {
-private Player currentPlayer;
-private ArrayList<Player> listOfPlayers;
+	private Player currentPlayer;
+	private ArrayList<Player> listOfPlayers;
 
-public String getCurrentPlayer() {
-		return Player.getName();
-}
+	public String getCurrentPlayer() {
+		return currentPlayer.getName();
+	}
 
-public void setCurrentPlayer(Player currentPlayer) {
-	this.currentPlayer = currentPlayer;
-}
+	private void initiatePlayerTurn() {
+		int playerTurn;
+		playerTurn = ThreadLocalRandom.current().nextInt(1, 5); //Selects from [1,5)
+		currentPlayer = listOfPlayers.get(playerTurn);
+	}
 
-private void initiatePlayerTurn() {
-	int playerTurn;
-	playerTurn = ThreadLocalRandom.current().nextInt(1, 5); //Selects from [1,5)
-	currentPlayer = listOfPlayers.get(playerTurn);
-}
+	public void setCurrentPlayer(Player currentPlayer) {
+		this.currentPlayer = currentPlayer;
+	}
 }
