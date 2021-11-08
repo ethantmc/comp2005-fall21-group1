7b1377256220574c9c796d94ff21e06fa7646249
ethantmc <77448566+ethantmc@users.noreply.github.com>
2021-11-08 15:45:25 -0330

    Turn enhanced, minor changes elsewhere.
    Focus.java also removed, as it was just a Driver class, but we ended up with an actual "Driver.java"

--- Group 1 Game of Focus/src/CPUPlayer.java
+++ Group 1 Game of Focus/src/CPUPlayer.java
@@ -1,10 +1,17 @@
 
 public class CPUPlayer extends Player {
-private DifficultyType difficulty;
+	private DifficultyType difficulty;
 
-public CPUPlayer(String name, PlayerType type, Boolean colorblindSetting, DifficultyType difficulty) {
-	super(name, type, colorblindSetting);
-	this.difficulty = difficulty;
-}
+	public CPUPlayer(String name, PlayerType type, Boolean colorblindSetting, DifficultyType difficulty) {
+		super(name, type, colorblindSetting);
+	}
+
+	public DifficultyType getDifficulty() {
+		return difficulty;
+	}
+
+	public void setDifficulty(DifficultyType difficulty) {
+		this.difficulty = difficulty;
+	}
 
 }
--- Group 1 Game of Focus/src/Turn.java
+++ Group 1 Game of Focus/src/Turn.java
@@ -2,20 +2,36 @@ import java.util.ArrayList;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Turn {
-	private Player currentPlayer;
+	private Player currentPlayer = null;
+	private int currentPlayerAsNumber;
 	private ArrayList<Player> listOfPlayers;
+
+	private void generateNextPlayer() {
+		currentPlayerAsNumber += 1;
+		currentPlayerAsNumber = ((currentPlayerAsNumber) % 4); //If the number would be 4, it loops around to 0 instead.
+		currentPlayer = listOfPlayers.get(currentPlayerAsNumber); //Retrieves the Player object in the space after the current player. 
+	}
 
 	public String getCurrentPlayer() {
 		return currentPlayer.getName();
 	}
 
-	private void initiatePlayerTurn() {
-		int playerTurn;
-		playerTurn = ThreadLocalRandom.current().nextInt(1, 5); //Selects from [1,5)
-		currentPlayer = listOfPlayers.get(playerTurn);
-	}
+	//	private void setCurrentPlayer(Player currentPlayer) { //The actual method for settings whose next. Should not be used, but may be necessary in a future iteration.
+	//		this.currentPlayer = currentPlayer;
+	//	}
 
-	public void setCurrentPlayer(Player currentPlayer) {
-		this.currentPlayer = currentPlayer;
+	public void initiatePlayerTurn() { //Only to be called the very first turn for the game.
+		int playerTurn;
+		playerTurn = ThreadLocalRandom.current().nextInt(0, 4); //Selects from [0,4)
+		currentPlayer = listOfPlayers.get(playerTurn);
+		currentPlayerAsNumber = playerTurn;
+	}
+	public void nextPlayersTurn() {
+		if (currentPlayer== null) {
+			initiatePlayerTurn();
+		}
+		else {
+			generateNextPlayer();
+		}
 	}
 }
