cb7c6b554167f6267e0308d9f5f40eccf3155b14
galibalfayed <66403971+galibalfayed@users.noreply.github.com>
2021-11-12 07:15:03 -0330

    Add files via upload

---
+++ Group 1 Iteration 3/Minor Release Java Files/src/Board.java
@@ -0,0 +1,20 @@
+
+public class Board {
+//TODO constructor and attributes for Board
+
+public void getTokensLeftForEachPlayer() {
+	
+//TODO Method Stub
+}
+public void getDominationPercentageForEachPlayer() {
+//TODO Method Stub
+}
+public void iiterateOverBoard() {
+	//TODO Method Stub
+	}
+public void calculatePossibleMoves(int coordinateX, int coordinateY) 
+{
+	
+}
+//TODO - IterateOverBoard() code for duplication in the above. After discussion, code duplication deemed preferable to IterateOverBoard() being a real method.
+}
---
+++ Group 1 Iteration 3/Minor Release Java Files/src/CPUPlayer.java
@@ -0,0 +1,17 @@
+
+public class CPUPlayer extends Player {
+	private DifficultyType difficulty;
+
+	public CPUPlayer(String name, PlayerType type, Boolean colorblindSetting, DifficultyType difficulty) {
+		super(name, type, colorblindSetting);
+	}
+
+	public DifficultyType getDifficulty() {
+		return difficulty;
+	}
+
+	public void setDifficulty(DifficultyType difficulty) {
+		this.difficulty = difficulty;
+	}
+
+}
