3236b3f48a67c1cf3eeff246131898a5d6ca02fd
galibalfayed <gafayed@mun.ca>
2021-11-29 03:40:20 -0330

    Move fixed.

--- Group 1 Game of Focus/src/Move.java
+++ Group 1 Game of Focus/src/Move.java
@@ -1,18 +1,20 @@
 
+import java.util.ArrayList;
+
 public class Move {
 
 	//private move
 	public static void makeAMove(Stack moveFrom, Stack moveTo, int num)
 	{
 		//TODO Method stub
-		moveFrom.moveTokens(num).forEach(i -> moveTo.stackToken(i));
+		moveFrom.moveTokens(num).forEach(i -> moveTo.stackToken(i)); //this code doesn't work, moveTokens returns a list...
 		Turn.nextPlayersTurn();
 		SetupAGame.getGameUIInstance().updateStats();
 	}
 
 	public static void makeAReserveMove(Stack moveToStack) 
 	{
-		
+
 		moveToStack.stackToken(Turn.getCurrentPlayer().getReserveToken()); //Driver.getPlayers().get(0).getReserveToken()
 		Turn.nextPlayersTurn();
 		SetupAGame.getGameUIInstance().updateStats();
