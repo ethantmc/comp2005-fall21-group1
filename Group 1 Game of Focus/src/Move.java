d78b4ffb967b9de9ffee95c0292b414a1b6e131e
ethantmc <77448566+ethantmc@users.noreply.github.com>
2021-11-29 10:59:01 -0330

    Documents for Final Iteration

--- Group 1 Game of Focus/src/Move.java
+++ Group 1 Game of Focus/src/Move.java
@@ -1,9 +1,8 @@
 public class Move {
 
-	//private move
+
 	public static void makeAMove(Stack moveFrom, Stack moveTo, int num)
 	{
-		//TODO Method stub
 		moveFrom.moveTokens(num).forEach(i -> moveTo.stackToken(i)); 
 		Turn.nextPlayersTurn();
 		SetupAGame.getGameUIInstance().updateStats();
